4415e1b0fdc4e55c9aa1a4ac28ac4795e4b3e4f8
Daniel Zammit <firepro20@gmail.com>
2022-06-07 23:56:36 +0100

    Submitted solution for Character Image
    Print switched rows and colomns

--- Chapter4.py
+++ Chapter4.py
@@ -76,10 +76,11 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-cols = []
-
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        print(grid[i][j], end='')
+#cols = []
+#print(len(grid))
+#print(len(grid[0]))
+for i in range(len(grid[0])):
+    for j in range(len(grid)):
+        print(grid[j][i], end='')
     print('\n')
 
