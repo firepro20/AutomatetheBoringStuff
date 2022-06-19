b6f95f43b2f38f13486e66172c652dd20df0d771
Daniel Zammit <firepro20@gmail.com>
2022-06-19 20:58:40 +0100

    Chapter 4 Questions

--- Chapter4.py
+++ Chapter4.py
@@ -84,3 +84,26 @@ for i in range(len(grid[0])):
         print(grid[j][i], end='')
     print('\n')
 
+# Questions for Chapter 4
+# 1. What is []? - an empty list
+# 2. How would you assign the value 'hello' as the third value in a list stored in a variable named spam? 
+# Assume spam contains ([2,4,6,8.10]) -  spam[2] = 'hello'
+# 3. d 
+# 4. d
+# 5. a b // excluding :2
+# 6. 1
+# 7. [3.14, 'cat', 11, 'cat', True, 99
+# 8. [3.14, 11, 'cat', True, 99]
+# 9. + * // question asked for operators
+# 10. appends add items at the end of the list; 
+# insert adds an item at index location
+# 11. del list[index] / list.remove(value)
+# 12. They both can be concatenated and replicated
+# sequences of data. ask for character via index
+# string can be accessed same way as list. string is a list of characters
+# in function works on lists and strings
+# 13. list are mutable and tuple are immutable
+# 14. mytuple = (42,)
+# 15. typecasting tuple(listvalue) / list(tuplevalue)
+# 16. references
+# 17. copies first layer of values, deepcopy copies lists within lists
